4041dad8134f89e8880b8341830f846600c1aa4f
Hai Bui <buiduchai277@gmail.com>
2025-10-24 16:07:28 +0700

    video 51

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from "react-router";
+import { NavLink, Link } from "react-router";
 
 const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Savio</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">Savio</Navbar.Brand> */}
+                <NavLink to="/" className='navbar-brand'>Savio</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link to="/" className='nav-link'>Home</Link>
-                        <Link to="/users" className='nav-link'>User</Link>
-                        <Link to="/admins" className='nav-link'>Admin</Link>
+                        <NavLink to="/" className='nav-link'>Home</NavLink>
+                        <NavLink to="/users" className='nav-link'>User</NavLink>
+                        <NavLink to="/admins" className='nav-link'>Admin</NavLink>
                         {/* <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">User</Nav.Link>
                         <Nav.Link href="#link">Admin</Nav.Link> */}
---
+++ src/components/Home/Homepage.js
@@ -0,0 +1,9 @@
+const Homepage = (props) => {
+    return (
+        <div className="homepage-container">
+            Homepage components
+        </div>
+    )
+}
+
+export default Homepage;
