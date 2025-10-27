886a995a0b2b08bfa0c9aa4fd346ea7b6935429c
Hai Bui <buiduchai277@gmail.com>
2025-10-27 11:47:03 +0700

    video 53

--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -1,7 +1,22 @@
+import videoHomepage from '../../assets/video-homepage.mp4'
+
 const Homepage = (props) => {
     return (
         <div className="homepage-container">
-            Homepage components
+            <video autoPlay muted loop>
+                <source
+                    src={videoHomepage}
+                    type="video/mp4"
+                ></source>
+            </video>
+            <div className='homepage-content'>
+                <div className='title-1'>Get 3.5x more data with a form expert</div>
+                <div className='title-2'>Backed by over a decade of experience,
+                    Typeform AI helps you build expertly-designed,
+                    best-practice forms proven to get more responses.</div>
+                <div className='title-3'>
+                    <button>See plans</button></div>
+            </div>
         </div>
     )
 }
